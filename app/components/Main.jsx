a06203d9f606a5a7ade02c05b483bb7f0a7a4745
vidurar <vidura.rajapaksa@gmail.com>
2017-05-26 12:08:36 +0800

    Initial Commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,17 @@
+var React = require("react");
+var Navigation = require('Navigation');
+
+
+var Main = (props)=>{
+  return(
+    <div>
+      <div>
+        <div>
+          <Navigation/>
+          <p>Main.jsx rendered</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+module.exports = Main;
